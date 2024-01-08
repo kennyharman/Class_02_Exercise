7a746895e1f2bf14914f9398a13084e2ec9290d6
Kenny Harman <kharman4@my.bcit.cagit config --global user.name kennyharman>
2024-01-08 12:12:12 -0800

    adding todays exercise to github

---
+++ pages/About.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header"
+import Counter from "@/components/Counter"
+
+export default function About() {
+    return (
+        <>
+            <Header/>
+            <main>
+                <Counter/>
+            </main>
+            test
+        </>
+    )
+}
